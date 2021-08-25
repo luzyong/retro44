91051ff2f34f3b5d401b9b2b436b2a9cd8af44d6
Luz Yong <32208374+luzyong@users.noreply.github.com>
2021-08-25 02:00:40 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 function conectar(){
 	$user="root";
-	$pass="lmyr1399";
+	$pass="";
 	$server="localhost";
 	$db="alumnos";
 	$con= mysqli_connect($server,$user,$pass,$db) or die ("Error en la conexión");
